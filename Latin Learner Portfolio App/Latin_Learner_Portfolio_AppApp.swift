2725dcb54e6603ce804af20ccb54badeaa70c08a
oliv3rwalsh <oliverwalsh45@gmail.com>
2022-12-02 08:31:40 -0600

    Initial Commit

---
+++ Latin Learner Portfolio App/Latin_Learner_Portfolio_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Latin_Learner_Portfolio_AppApp.swift
+//  Latin Learner Portfolio App
+//
+//  Created by Oliver Walsh on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Latin_Learner_Portfolio_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
