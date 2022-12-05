7fe928286289f3207b22b488ee99aed3b5cff82a
oliv3rwalsh <oliverwalsh45@gmail.com>
2022-12-05 15:18:36 -0600

    updated sign out procedure

--- Latin Learner Portfolio App/ContentView.swift
+++ Latin Learner Portfolio App/ContentView.swift
@@ -13,43 +13,74 @@ struct ContentView: View {
     
     var body: some View {
         if(vm.isLoggedIn){
-            VStack{
-                HStack{
-                    Spacer()
-                    AsyncImage(
-                        url: URL(string: vm.profilePicUrl),
-                        content: { image in
-                            image.resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: 45, maxHeight: 45)
-                                .cornerRadius(90.0)
-                        },
-                        placeholder: {
-                            Image(systemName: "person.circle.fill").font(.system(size: 45))
-                        }
-                    )
-                    Spacer()
-                    Text("Signed in as " + vm.givenName).mediumText()
-                    Spacer()
-                    Button(action: {vm.signOut()}){
-                        Text("Sign Out").mediumText()
-                            .padding(5)
-                    }.signOutButton()
-                    Spacer()
-                }
-                Spacer()
-            }
+            mainScreen()
         } else {
-            VStack{
+            loginScreen()
+        }
+    }
+}
+
+struct loginScreen: View {
+    @EnvironmentObject var vm: UserAuthModel
+    var body: some View {
+        VStack{
+            Spacer()
+            Text("You must be signed in to access your Latin Learner Portfolio").largeText()
+                .multilineTextAlignment(.center)
+            Button(action: {vm.signIn()}){
+                Text("Sign in with Google").mediumText()
+                    .padding(10)
+            }.signInButton()
+            Spacer()
+        }
+    }
+}
+
+struct mainScreen: View {
+    @EnvironmentObject var vm: UserAuthModel
+    @State private var showingAlert = false
+    var body: some View {
+        VStack{
+            HStack{
                 Spacer()
-                Text("You must be signed in to access your Latin Learner Portfolio").largeText()
-                    .multilineTextAlignment(.center)
-                Button(action: {vm.signIn()}){
-                    Text("Sign in with Google").mediumText()
-                        .padding(10)
-                }.signInButton()
+                AsyncImage(
+                    url: URL(string: vm.profilePicUrl),
+                    content: { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 45, maxHeight: 45)
+                            .cornerRadius(90.0)
+                    },
+                    placeholder: {
+                        Image(systemName: "person.circle.fill").font(.system(size: 45))
+                    }
+                )
+                Spacer()
+                Text("Signed in as " + vm.givenName).mediumText()
+                Spacer()
+                
+                
+                
+                Button(action: {showingAlert = true}){
+                    Image(systemName: "person.fill.xmark").font(.system(size: 25))
+                }
+                .foregroundColor(Color(.black))
+                .alert(isPresented:$showingAlert) {
+                    Alert(
+                        title: Text("Do you want to sign out?"),
+                        message: Text("You will not be able to access your Latin Learner Portfolio if you are not signed into your Google account"),
+                        primaryButton: .default(Text("Sign Out")) {
+                            vm.signOut()
+                        },
+                        secondaryButton: .cancel()
+                    )
+                }
+                
+                
+                
                 Spacer()
             }
+            Spacer()
         }
     }
 }
