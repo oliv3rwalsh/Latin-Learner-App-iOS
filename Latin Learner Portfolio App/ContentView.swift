6fe4bff1532ee4ef15475360163304c39a988711
oliv3rwalsh <oliverwalsh45@gmail.com>
2022-12-08 12:46:05 -0600

    moved parts of files over to resources files

--- Latin Learner Portfolio App/ContentView.swift
+++ Latin Learner Portfolio App/ContentView.swift
@@ -10,6 +10,7 @@ import GoogleSignIn
 import GoogleSignInSwift
 
 let salutations = ["Hey there", "Welcome", "What's up", "Hello"]
+let chosenSalutation = salutations.randomElement()!
 
 struct ContentView: View {
     @EnvironmentObject var vm: UserAuthModel
@@ -19,7 +20,7 @@ struct ContentView: View {
             VStack{
                 topbar()
                 Spacer().frame(height: 25)
-                pageContent()
+                homePageContent()
             }.ignoresSafeArea()
         } else {
             loginScreen()
@@ -27,90 +28,22 @@ struct ContentView: View {
     }
 }
 
-struct loginScreen: View {
+struct homePageContent: View {
     @EnvironmentObject var vm: UserAuthModel
     var body: some View {
         VStack{
             Spacer()
-            Image(systemName: "person.fill.questionmark").font(.system(size: 100))
-            Spacer().frame(height: 40)
-            Text("You must be signed in to access your Latin Learner Portfolio").largeText()
-                .multilineTextAlignment(.center)
-//            GoogleSignInButton(action: {vm.signIn()})
-            Spacer().frame(height: 20)
-            Button(action: {vm.signIn()}){
-                Text("Sign in with Google").mediumText()
-                    .padding(10)
-            }.signInButton()
-            Spacer()
-        }
-    }
-}
-
-struct topbar: View {
-    @EnvironmentObject var vm: UserAuthModel
-    @State private var showingAlert = false
-    var body: some View {
-        VStack{
-            Spacer().frame(height: 60)
             HStack{
                 Spacer()
-                AsyncImage(
-                    url: URL(string: vm.profilePicUrl),
-                    content: { image in
-                        image.resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 45, maxHeight: 45)
-                            .cornerRadius(90.0)
-                    },
-                    placeholder: {
-                        Image(systemName: "person.circle.fill").font(.system(size: 45))
-                    }
-                )
-                Spacer()
-                // Text("Signed in as " + vm.givenName).mediumText()
-                Text("Latin Learner Portfolio").mediumText()
-                Spacer()
-                signOutButton()
+                Text("ADD CONTENT HERE").mediumText()
                 Spacer()
             }
-            Spacer().frame(height: 20)
-        }.bar()
-    }
-}
-
-struct signOutButton: View {
-    @EnvironmentObject var vm: UserAuthModel
-    @State private var showingAlert = false
-    var body: some View {
-        Button(action: {showingAlert = true}){
-            Image(systemName: "person.fill.xmark").font(.system(size: 25))
-        }
-        .foregroundColor(Color(.black))
-        .alert(isPresented:$showingAlert) {
-            Alert(
-                title: Text("Do you want to sign out?"),
-                message: Text("You will not be able to access your Latin Learner Portfolio if you are not signed into your Google account"),
-                primaryButton: .default(Text("Sign Out")) {
-                    vm.signOut()
-                },
-                secondaryButton: .cancel()
-            )
-        }
-    }
-}
-
-struct pageContent: View {
-    @EnvironmentObject var vm: UserAuthModel
-    var body: some View {
-        VStack{
-            Spacer().frame(height: 50)
             HStack{
-                Spacer().frame(width: 30)
-                Text(salutations.randomElement()! + ", " + vm.givenName).largeText()
+                Spacer()
+                Text("HOME PAGE").smallText()
                 Spacer()
             }
             Spacer()
-        }
+        }.background(Color("Google Blue"))
     }
 }
---
+++ Latin Learner Portfolio App/Resources.swift
@@ -0,0 +1,81 @@
+//
+//  Resources.swift
+//  Latin Learner Portfolio App
+//
+//  Created by Oliver Walsh on 12/7/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct loginScreen: View {
+    @EnvironmentObject var vm: UserAuthModel
+    var body: some View {
+        VStack{
+            Spacer()
+            Image(systemName: "person.fill.questionmark").font(.system(size: 100))
+            Spacer().frame(height: 40)
+            Text("You must be signed in to access your Latin Learner Portfolio").largeText()
+                .multilineTextAlignment(.center)
+//            GoogleSignInButton(action: {vm.signIn()})
+            Spacer().frame(height: 20)
+            Button(action: {vm.signIn()}){
+                Text("Sign in with Google").mediumText()
+                    .padding(10)
+            }.signInButton()
+            Spacer()
+        }
+    }
+}
+
+struct topbar: View {
+    @EnvironmentObject var vm: UserAuthModel
+    @State private var showingAlert = false
+    var body: some View {
+        VStack{
+            Spacer().frame(height: 60)
+            HStack{
+                Spacer()
+                AsyncImage(
+                    url: URL(string: vm.profilePicUrl),
+                    content: { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 45, maxHeight: 45)
+                            .cornerRadius(90.0)
+                    },
+                    placeholder: {
+                        Image(systemName: "person.circle.fill").font(.system(size: 45))
+                    }
+                )
+                Spacer()
+                Text(chosenSalutation + ", " + vm.givenName).mediumText()
+                Spacer()
+                signOutButton()
+                Spacer()
+            }
+            Spacer().frame(height: 20)
+        }.bar()
+    }
+}
+
+struct signOutButton: View {
+    @EnvironmentObject var vm: UserAuthModel
+    @State private var showingAlert = false
+    var body: some View {
+        Button(action: {showingAlert = true}){
+            Image(systemName: "person.fill.xmark").font(.system(size: 25))
+        }
+        .foregroundColor(Color(.black))
+        .alert(isPresented:$showingAlert) {
+            Alert(
+                title: Text("Do you want to sign out?"),
+                message: Text("You will not be able to access your Latin Learner Portfolio if you are not signed into your Google account"),
+                primaryButton: .default(Text("Sign Out")) {
+                    vm.signOut()
+                },
+                secondaryButton: .cancel()
+            )
+        }
+    }
+}
